332e97f21ff8d7ed3ad23c9413f1251598cc25d9
Adam B. Smith <adam@earthskysea.org>
2018-07-06 12:03:28 -0500

    Restore lost functions

--- R/convertTropicosCoords.r
+++ R/convertTropicosCoords.r
@@ -1,18 +1,18 @@
 #' Convert coordinates from TROPICOS records to decimal degrees.
 #'
-#' The [TROPICOS](http://www.tropicos.org/) plant specimen database of the Missouri Botanical Garden returns records in degrees-minutes-seconds format with symbols for degrees, minutes, second, and hemisphere.  This function converts this format to signed decimal degrees format.  By default, values surrounded by square brackets (\code{[]}) are returned as \code{NA} because they are geolocated to the center of a political unit (usually county or equivalent).
+#' The TROPICOS (http://www.tropicos.org/) plant specimen database of the Missouri Botanical Garden returns records in degrees-minutes-seconds format with symbols for degrees, minutes, second, and hemisphere.  This function converts this format to signed decimal degrees format.  By default, values surrounded by square brackets (\code{[]}) are returned as \code{NA} because they are geolocated to the center of a political unit (usually county or equivalent).
 #' @param long Character list, longitudes in TROPICOS format.
 #' @param lat Character list, latitudes in TROPICOS format.
 #' @param returnApprox Logical, if \code{TRUE} then approximate coordinates (surrounded by square brackets in TROPICOS format) are converted. If \code{FALSE} (default), then \code{NA} is returned.
 #' @return A data five-column frame. The first two columns are the original TROPICOS coordinates, the next two the converted coordinates, and the last a flag to indicate if the TROPICOS coordinates were approximate (\code{TRUE}) or "exact" (\code{FALSE}).
-#' @seealso \code{\link[enmSdm]{ddmmssToDecimal}}
+#' @seealso \code{\link[enmSdm]{dmsToDecimal}}
 #' @examples
 #' long <- c("078°14'05\"\"W", "091°49'39\"\"W", NA, "[091°53'38\"\"W]")
 #' lat <- c("39°40'41\"\"N", "36°34'14\"\"S", NA, "[36°46'14\"\"N]")
-#' tropicosCoordConvert(long, lat)
-#' tropicosCoordConvert(long, lat, TRUE)
+#' convertTropicosCoords(long, lat)
+#' convertTropicosCoords(long, lat, TRUE)
 #' @export
-tropicosCoordConvert <- function(
+convertTropicosCoords <- function(
 	long,
 	lat,
 	returnApprox = FALSE
@@ -65,28 +65,8 @@ tropicosCoordConvert <- function(
 			thisLong <- unlist(strsplit(thisLong, split='\"'))
 			thisLat <- unlist(strsplit(thisLat, split='\"'))
 
-			ew <- if (thisLong[5] == 'W') {
-				-1
-			} else {
-				1
-			}
-
-			ns <- if (thisLat[5] == 'S') {
-				-1
-			} else {
-				1
-			}
-
-			thisLong <- as.numeric(thisLong[1]) +
-				as.numeric(thisLong[2]) / 60 +
-				as.numeric(thisLong[3]) / 3600
-
-			thisLat <- as.numeric(thisLat[1]) +
-				as.numeric(thisLat[2]) / 60 +
-				as.numeric(thisLat[3]) / 3600
-
-			thisLong <- ew * thisLong
-			thisLat <- ns * thisLat
+			thisLong <- dmsToDecimal(as.numeric(thisLong[1]), as.numeric(thisLong[2]), as.numeric(thisLong[3]), thisLong[5])
+			thisLat <- dmsToDecimal(as.numeric(thisLat[1]), as.numeric(thisLat[2]), as.numeric(thisLat[3]), thisLat[5])
 
 			out$longitude[i] <- thisLong
 			out$latitude[i] <- thisLat
