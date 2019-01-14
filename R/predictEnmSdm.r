618848bcb8e34bcc374ae12534bd8a04e11e2d1b
Adam B. Smith <adam@earthskysea.org>
2019-01-14 13:17:12 -0600

    Update help

--- R/predictEnmSdm.r
+++ R/predictEnmSdm.r
@@ -1,12 +1,12 @@
 #' Generic predict function for LMs, GLMs, GAMs, RFs, BRTs, CRFs, Maxent, and more
 #'
-#' This is a generic predict function that automatically uses the model common arguments for predicting models of the following types: linear models, generalized linear models (GLMs), generalzied additive models (GAMs), random forests, boosted regression trees (BRTs)/gradient boosting machines (GBMs), conditional random forests, Maxent, and more.
-#' @param model  Object of class \code{lm}, \code{glm}, \code{gam}, \code{randomForest}, \code{xxxx}, \code{MaxEnt}, \code{MaxNet}, and possibly others (worth a try!).
+#' This is a generic predict function that automatically uses the model common arguments for predicting models of the following types: linear models, generalized linear models (GLMs), generalized additive models (GAMs), random forests, boosted regression trees (BRTs)/gradient boosting machines (GBMs), conditional random forests, Maxent, and more.
+#' @param model  Object of class \code{lm}, \code{glm}, \code{gam}, \code{randomForest}, \code{MaxEnt}, \code{MaxNet}, and possibly others (worth a try!).
 #' @param newdata Data frame or matrix with data to which to predict
 #' @param maxentFun Either \code{'dismo'} in which case a Maxent model is predicted using the default \code{predict} function from the \pkg{dismo} package, or \code{'enmSdm'} in which case the function \code{\link[enmSdm]{predictMaxEnt}} function from the \pkg{enmSdm} package is used.
 #' @param ... Arguments to pass to the algorithm-specific \code{predict} function.
 #' @return Numeric.
-#' @seealso \code{\link[stats]{predict}} from the stats package, \code{\link[dismo]{predict}} from the dismmo package, \code{\link[raster]{predict}} fromm the raster package
+#' @seealso \code{\link[stats]{predict}} from the stats package, \code{\link[dismo]{predict}} from the dismo package, \code{\link[raster]{predict}} fromm the raster package
 #' @examples
 #' 
 #' @export
