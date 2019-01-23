047156cfde31e095fe7258fe2760e95f0edd795c
Adam B. Smith <adam@earthskysea.org>
2019-01-23 11:11:57 -0600

    Add package help page

---
+++ R/enmSdm.r
@@ -0,0 +1,10 @@
+#' enmSdm: Species distribution modeling and ecological niche modeling
+#'
+#' This package contains tools for modeling the distributions and niches of species or species-like entities. Its main features are a set of functions for training ENMs, evaluating niche overlap, and correcting for sampling bias.
+#'
+#' Create an issue on \href{https://github.com/adamlilith/enmSdm/issues}{GitHub}.
+#'
+#' @docType package
+#' @author Adam B. Smith
+#' @name enmSdm
+NULL
