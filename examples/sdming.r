9662bc7aa55437c6bd65622c47d417c3c0085a19
Adam B. Smith <adam@earthskysea.org>
2019-06-19 16:51:04 -0500

    Update sdming.r

--- examples/sdming.r
+++ examples/sdming.r
@@ -9,16 +9,16 @@ y <- 2 * x1 + x1^2 - 10 * x2 - x1 * x2
 
 y <- statisfactory::probitAdj(y, 0)
 y <- y^3
-hist(y)
 
 presAbs <- runif(n) < y
 
 trainData <- data.frame(presAbs=presAbs, x1=x1, x2=x2, x3=x3)
 
-model <- trainGlm(trainData)
+# source('C:/Ecology/Drive/R/enmSdm/R/trainGlm.r')
+model <- trainGlm(trainData, out=c('model', 'models', 'tuning'))
 
-source('C:/Ecology/Drive/R/enmSdm/working/trainByCrossValid.r')
-out <- trainByCrossValid(data=trainData, verbose=2)
+source('C:/Ecology/Drive/R/enmSdm/R/trainByCrossValid.r')
+out <- trainByCrossValid(data=trainData, verbose=1)
 
 
 
@@ -41,5 +41,7 @@ out <- trainByCrossValid(data=trainData, verbose=2)
 #' trainData <- data.frame(presAbs=presAbs, x1=x1, x2=x2, x3=x3)
 #' 
 #' model <- trainGlm(trainData)
+#' summary(model)
 #' 
-#' out <- trainByCrossValid(data=trainData, verbose=2)
+#' out <- trainByCrossValid(data=trainData, verbose=1)
+#' out$tuning
