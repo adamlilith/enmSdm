5f0ca637e062e520c601df04a65ef0fccae3e07c
Adam B. Smith <adam@earthskysea.org>
2019-09-18 12:00:30 -0500

    Fix bug in geoThin() in cases where there were no points to remove

--- R/geoThin.r
+++ R/geoThin.r
@@ -5,7 +5,6 @@
 #' @param minDist Numeric. Minimum distance needed between points to retain them. Points falling < this distance will be discarded. If \code{distFunct} is \code{distGeo} then this should be in the same units as \code{f} (see \code{link[geosphere]{distGeo}} and related "\code{dist}" functions).
 #' @param longLat Two-element character list \emph{or} two-element integer list. If \code{x} is a data frame then this should be a character list specifiying the names of the fields in \code{x} \emph{or} a two-element list of integers that correspond to longitude and latitude (in that order). For example, \code{c('long', 'lat')} or \code{c(1, 2)}. If \code{x} is a matrix then this is a two-element list indicating the column numbers in \code{x} that represent longitude and latitude (for example, \code{c(1, 2)}). If \code{x} is a \code{SpatialPoints} or a \code{SpatialPointsDataFrame}  object then this argument is ignored.
 #' @param distFunct Either a function or \code{NULL}. If \code{NULL} then \code{\link[geosphere]{distGeo}} is used to calculate distances.  More accurate distances can be obtained by using other functions (see \code{\link[geosphere]{distHaversine}} and references therein). Alternatively, a custom function can be used so long as its first argument is a 2-column numeric matrix with one row for the x- and y-coordinates of a single point and its second argument is a two-column numeric matrix with one or more rows of other points.
-#' @param na.rm Logical. If \code{TRUE} then remove coordinates with \code{NA}s first.
 #' @param verbose Logical. If \code{TRUE} then display progress.
 #' @param ... Arguments to pass to \code{distFunct}.
 #' @details
@@ -25,20 +24,37 @@
 #' # example using data frame
 #' x <- data.frame(long=c(-90.1, -90.1, -90.15, -90.17, -90.2, -89),
 #'    lat=c(38, 38, 38, 38, 38, 38), point=letters[1:6])
-#' geoThin(x, minDist=500, longLat=c(1, 2))
-#' geoThin(x, minDist=5000, longLat=c(1, 2))
+#' x
+#' geoThin(x, minDist=500, longLat=1:2, verbose=TRUE)
+#' geoThin(x, minDist=5000, longLat=c(1, 2), verbose=TRUE)
+#'
 #' # example of potential randomness
-#' set.seed(123)
+#' set.seed(111)
 #' geoThin(x, minDist=1000, longLat=c(1, 2))
 #' geoThin(x, minDist=1000, longLat=c(1, 2))
 #' geoThin(x, minDist=1000, longLat=c(1, 2))
 #'
 #' # example using SpatialPointsDataFrame
-#' data(lemur)
+#' data(lemurs)
+#' fulvus <- lemurs[lemurs$species == 'Eulemur fulvus', c('longitude', 'latitude')]
+#' fulvus <- sp::SpatialPointsDataFrame(
+#' 		fulvus,
+#' 		data=fulvus,
+#' 		proj4string=getCRS('wgs84', TRUE)
+#' )
+#'
 #' data(mad0)
-#' plot(mad0, main='Madagascar')
-#' points(lemur, col='red')
-#' thinned <- geoThin(lemur, 50000)
+#' sp::plot(mad0, main='Madagascar')
+#' points(fulvus, col='red')
+#' thinned <- geoThin(fulvus, 50000)
+#' points(thinned, pch=16)
+#' legend('topright', legend=c('retained', 'discarded'),
+#' col=c('black', 'red'), pch=c(16, 1))
+#'
+#' # test to see function works when no points need removed
+#' thinned <- geoThin(fulvus, 200, verbose=TRUE)
+#' sp::plot(mad0, main='Madagascar')
+#' points(fulvus, col='red')
 #' points(thinned, pch=16)
 #' legend('topright', legend=c('retained', 'discarded'),
 #' col=c('black', 'red'), pch=c(16, 1))
@@ -49,7 +65,6 @@ geoThin <- function(
 	minDist,
 	longLat = NULL,
 	distFunct = NULL,
-	na.rm = TRUE,
 	verbose = FALSE,
 	...
 ) {
@@ -70,56 +85,60 @@ geoThin <- function(
 	# neighbors
 	neighs <- colSums(dists <= minDist, na.rm=TRUE)
 
-	# while there is at least one point with neighbors too close
-	if (verbose) {
-		omnibus::say('Thinning points...')
-		totalNeighs <- sum(neighs)
-		prog <- utils::txtProgressBar(min=0, max=totalNeighs, width=32, style=3)
-	}
+	if (any(neighs > 0)) {
+	
+		# while there is at least one point with neighbors too close
+		if (verbose) {
+			omnibus::say('Thinning points...')
+			totalNeighs <- sum(neighs)
+			prog <- utils::txtProgressBar(min=0, max=totalNeighs, width=32, style=3)
+		}
 
-	while (any(neighs > 0)) {
+		while (any(neighs > 0)) {
 
-		# get points with greatest number of neighbors
-		maxNeighs <- max(neighs)
-		removeThis <- which(neighs == maxNeighs)
+			# get points with greatest number of neighbors
+			maxNeighs <- max(neighs)
+			removeThis <- which(neighs == maxNeighs)
 
-		# if more than one point has the maximum number of neighbors, break ties
-		if (length(removeThis) > 1) {
-
-			# removing point with closest neighbor
-			distClosestNeigh <- apply(dists[ , removeThis], 2, min)
-			minDistClosestNeigh <- min(distClosestNeigh)
-			
-			# catch cases where points are all at same coordinates, remove one randomly
-			removeThis <- if (all(minDistClosestNeigh == 0)) {
-				removeThis[sample(seq_along(removeThis), 1)]
-			} else {
-				removeThis[which(distClosestNeigh == minDistClosestNeigh)]
-			}
-
-			# if there is still a tie, break ties
+			# if more than one point has the maximum number of neighbors, break ties
 			if (length(removeThis) > 1) {
 
-				# break ties by removing point closest to centroid of all remaining points
-				cent <- rgeos::gCentroid(xy[index])
-				distToCent <- distFunct(cent, xy[removeThis], ...)
-				minDistToCent <- min(distToCent)
+				# removing point with closest neighbor
+				distClosestNeigh <- apply(dists[ , removeThis], 2, min)
+				minDistClosestNeigh <- min(distClosestNeigh)
+				
+				# catch cases where points are all at same coordinates, remove one randomly
+				removeThis <- if (all(minDistClosestNeigh == 0)) {
+					removeThis[sample(seq_along(removeThis), 1)]
+				} else {
+					removeThis[which(distClosestNeigh == minDistClosestNeigh)]
+				}
 
-				removeThis <- removeThis[which(distToCent == minDistToCent)]
-
-				# if there is *still* a tie, break ties
+				# if there is still a tie, break ties
 				if (length(removeThis) > 1) {
 
-					# break ties by removing point with least median distance to all other points (even if > minDist away)
-					distToAllPoints <- apply(dists[ , removeThis], 2, median, na.rm=TRUE)
-					minMedianDistToAllPoints <- min(distToAllPoints)
-					removeThis <- removeThis[which(distToAllPoints == minMedianDistToAllPoints)]
+					# break ties by removing point closest to centroid of all remaining points
+					cent <- rgeos::gCentroid(xy[index])
+					distToCent <- distFunct(cent, xy[removeThis], ...)
+					minDistToCent <- min(distToCent)
+
+					removeThis <- removeThis[which(distToCent == minDistToCent)]
 
 					# if there is *still* a tie, break ties
 					if (length(removeThis) > 1) {
 
-						# break ties by randomly selecting a point
-						removeThis <- removeThis[sample(seq_along(removeThis), 1)]
+						# break ties by removing point with least median distance to all other points (even if > minDist away)
+						distToAllPoints <- apply(dists[ , removeThis], 2, median, na.rm=TRUE)
+						minMedianDistToAllPoints <- min(distToAllPoints)
+						removeThis <- removeThis[which(distToAllPoints == minMedianDistToAllPoints)]
+
+						# if there is *still* a tie, break ties
+						if (length(removeThis) > 1) {
+
+							# break ties by randomly selecting a point
+							removeThis <- removeThis[sample(seq_along(removeThis), 1)]
+
+						}
 
 					}
 
@@ -127,34 +146,37 @@ geoThin <- function(
 
 			}
 
+			# remove point index from those to keep
+			index <- index[-which(index == removeThis)]
+			
+			dists[removeThis, ] <- rep(Inf, ncol(dists))
+			dists[ , removeThis] <- rep(Inf, nrow(dists))
+
+			# neighbors
+			neighs <- colSums(dists <= minDist, na.rm=TRUE)
+
+			if (verbose) utils::setTxtProgressBar(prog, totalNeighs - sum(neighs))
+
 		}
 
-		# remove point index from those to keep
-		index <- index[-which(index == removeThis)]
-		
-		dists[removeThis, ] <- rep(Inf, ncol(dists))
-		dists[ , removeThis] <- rep(Inf, nrow(dists))
+		if (verbose) close(prog)
 
-		# neighbors
-		neighs <- colSums(dists <= minDist, na.rm=TRUE)
+		# remove too-neighborly points
+		x <- if (class(x) == 'data.frame' | class(x) == 'matrix') {
+			x[index, ]
+		} else if (class(x) == 'SpatialPointsDataFrame') {
+			x[index, ]
+		} else {
+			x[index]
+		}
 
-		if (verbose) utils::setTxtProgressBar(prog, totalNeighs - sum(neighs))
-
-	}
-
-	if (verbose) close(prog)
-
-	# remove too-neighborly points
-	x <- if (class(x) == 'data.frame' | class(x) == 'matrix') {
-		x[index, ]
-	} else if (class(x) == 'SpatialPointsDataFrame') {
-		x[index, ]
+	# all pairwise distances are > minimum distance
 	} else {
-		x[index]
+	
+		if (verbose) omnibus::say('No sites were within the minimum distance, so none were removed.')
+		
 	}
 
 	x
 
-
 }
-
