f52ee42bed8241f5aec1dec0bf0425e0cb89134d
Adam B. Smith <adam@earthskysea.org>
2020-10-20 10:51:29 -0500

    Speed improvements for bioticVelocity(); better interpolation of quantile velocities; can account for elevation

--- R/bioticVelocity.r
+++ R/bioticVelocity.r
@@ -21,10 +21,6 @@
 #' @param elevation Either \code{NULL} (default) or a raster or matrix representing elevation. If this is supplied, changes in elevation are incorporated into all velocity and speed metrics. Additionally, you can also calculate the metrics \code{elevCentrioid} and \code{elevQuants}.
 #' @param metrics Biotic velocity metrics to calculate (default is to calculate them all). All metrics ignore \code{NA} cells in \code{x}. Here "starting time period" represents one layer in \code{x} and "end time period" the next layer.
 #' \itemize{
-#' 	\item \code{centroid}: Speed of mass-weighted centroid.
-#'  \item \code{nsCentroid} or \code{ewCentroid}: Velocity in the north-south or east-west directions of the mass-weighted centroid. For north-south cardinality, positive values represent movement northward and negative southward.
-#'  \item \code{nCentroid}, \code{sCentroid}, \code{eCentroid}, and \code{wCentroid}: Speed of mass-weighted centroid of the portion of the raster north/south/east/west of the landscape-wide weighted centroid of the starting time period.
-#'  \item \code{nsQuants} or \code{ewQuants}: Velocity of the location of the \emph{N}th quantile of mass in the north-south or east-west directions. The quantiles can be specified in \code{quants}. For example, this could be the movement of the 5th, 50th, and 95th quantiles of population size going from south to north. The 0th quantile would measure the velocity of the southernmost or easternmost cell(s) with values >0, and the 100th quantile the northernmost or westernmost cell(s) with non-zero values.
 #'  \item \code{summary}: This calculates a series of measures, none of which are measures of velocity:
 #'		\itemize{
 #'			\item Mean: Mean value across all cells.
@@ -32,6 +28,10 @@
 #'			\item Quantiles: \emph{N}th quantile values across all cells. Quantiles are given by \code{quants}.
 #'			\item Prevalence: Number of cells with values > 0.
 #'		}
+#' 	\item \code{centroid}: Speed of mass-weighted centroid.
+#'  \item \code{nsCentroid} or \code{ewCentroid}: Velocity in the north-south or east-west directions of the mass-weighted centroid. For north-south cardinality, positive values represent movement northward and negative southward.
+#'  \item \code{nCentroid}, \code{sCentroid}, \code{eCentroid}, and \code{wCentroid}: Speed of mass-weighted centroid of the portion of the raster north/south/east/west of the landscape-wide weighted centroid of the starting time period.
+#'  \item \code{nsQuants} or \code{ewQuants}: Velocity of the location of the \emph{N}th quantile of mass in the north-south or east-west directions. The quantiles can be specified in \code{quants}. For example, this could be the movement of the 5th, 50th, and 95th quantiles of population size going from south to north. The 0th quantile would measure the velocity of the southernmost or easternmost cell(s) with values >0, and the 100th quantile the northernmost or westernmost cell(s) with non-zero values.
 #'  \item \code{similarity}: Several metrics of similarity between each time period. Some of these make sense only for cases where values in \code{x} are in the range [0, 1], but not if some values are outside this range. The metrics are:
 #'		\itemize{
 #'			\item Simple mean difference
@@ -54,22 +54,22 @@
 #' 	\item \code{timeFrom}: Start time of interval
 #' 	\item \code{timeTo}: End time of interval
 #' 	\item \code{timeSpan}: Duration of interval
-#' 	\item \code{propSharedCellsNotNA}: Proportion of cells that were not \code{NA} in both starting and ending time periods
-#' 	\item \code{timeFromPropNotNA} and \code{timeToPropNotNA}: Proportion of cells that were not \code{NA} in the starting and ending time periods
 #' }
 #' Depending on \code{metrics} that are specified, additional fields are as follows. All measurements of velocity are in distance units (typically meters) per time unit (which is the same as the units used for \code{times} and \code{atTimes}). For example, if the rasters are in an Albers equal-area projection and \code{times} are in years, then the output will be meters per year.
 #' \itemize{
+#' 	\item If \code{metrics} contains \code{summary}:
+#' 	\itemize{
+#'		\item A column named \code{propSharedCellsNotNA}: Proportion of cells that are not \code{NA} in both the "from" and "to" time step.
+#'		\item Columns named \code{timeFromPropNotNA} and \code{timeToPropNotNA}: Proportion of cells in the "from" time and "to" steps that are not \code{NA}.
+#' 		\item A column named \code{mean}: Mean weight in "timeTo" time step. In the same units as the values of the cells.
+#' 		\item Columns named \code{quantile_quant\emph{N}}: The \emph{N}th quantile(s) of weight in the "timeTo" time step. In the same units as the values of the cells.
+#' 		\item A column named \code{prevalence}: Proportion of non-\code{NA} cells with weight >0 in the "timeTo" time step relative to all non-\code{NA} cells. Unitless.
+#' }
 #' 	\item If \code{metrics} has \code{'centroid'}: Columns named \code{centroidVelocity}, \code{centroidLong}, \code{centroidLat} -- Speed of weighted centroid, plus its longitude and latitude (in the "to" time period of each time step). Values are always >= 0.
 #' 	\item If \code{metrics} has \code{'nsCentroid'}: Columns named \code{nsCentroid} and \code{nsCentroidLat} -- Velocity of weighted centroid in north-south direction, plus its latitude (in the "to" time period of each time step). Positive values connote movement north, and negative values south.
 #' 	\item If \code{metrics} has \code{'ewControid'}: \code{ewCentroid} and \code{ewCentroidLong} -- Velocity of weighted centroid in east-west direction, plus its longitude (in the "to" time period of each time step).  Positive values connote movement east, and negative values west.
 #' 	\item If \code{metrics} has \code{'nCentroid'}, \code{'sCentroid'}, \code{'eCentroid'}, and/or \code{'wCentroid'}: Columns named \code{nCentroidVelocity} and \code{nCentroidAbund}, \code{sCentroid} and \code{sCentroidAbund}, \code{eCentroid} and \code{eCentroidAbund}, and/or \code{wCentroid} and \code{wCentroidAbund} -- Speed of weighted centroid of all cells that fall north, south, east, or west of the landscape-wide centroid, plus a column indicating the weight (abundance) of all such populations. Values are always >= 0.
 #' 	\item If \code{metrics} contains any of \code{nsQuants} or \code{ewQuants}: Columns named \code{nsQuantVelocity_quant\emph{N}} and \code{nsQuantLat_quant\emph{N}}, or \code{ewQuantVelocity_quant\emph{N}} and \code{ewQuantLat_quant\emph{N}}: Velocity of the \emph{N}th quantile weight in the north-south or east-west directions, plus the latitude or longitude thereof (in the "to" time period of each time step). Quantiles are cumulated starting from the south or the west, so the 0.05th quantile, for example, is in the far south or west of the range and the 0.95th in the far north or east. Positive values connote movement north or east, and negative values movement south or west.
-#' 	\item If \code{metrics} contains \code{summary}:
-#' 	\itemize{
-#' 		\item A column named \code{mean}: Mean weight in "timeTo" time step. In the same units as the values of the cells.
-#' 		\item Columns named \code{quantile_quant\emph{N}}: The \emph{N}th quantile(s) of weight in the "timeTo" time step. In the same units as the values of the cells.
-#' 		\item A column named \code{prevalence}: Proportion of non-\code{NA} cells with weight >0 in the "timeTo" time step relative to all non-\code{NA} cells. Unitless.
-#' }
 #' \item If \code{metrics} contains \code{similarity}, metrics of similarity are calculated for each pair of successive landscapes, defined below as \code{x1} and \code{x2}, with the number of shared non-\code{NA} cells between them being \code{n}:
 #'	\itemize{
 #'		\item A column named \code{simpleMeanDiff}: \code{sum(x2 - x1, na.rm=TRUE) / n}
@@ -326,11 +326,19 @@
 #' \donttest{
 #' ### multi-core
 #' mats <- array(runif(100 * 100 * 1000), dim=c(100, 100, 500))
-#' 
 #' mats <- brick(mats)
 #' projection(mats) <- getCRS('wgs84')
-#' 
-#' mc <- bioticVelocity(x=mats, metrics='centroid', cores=4)
+#'
+#' # adding elevation increases velocity because it increases average
+#' # distance between cells
+#' elev <- matrix(rnorm(100 * 100, 0, 0.1), nrow=100)
+#' elev <- raster(elev, crs=getCRS('wgs84'))
+#'
+#' mc1 <- bioticVelocity(x=mats, metrics='centroid', cores=4)
+#' mc2 <- bioticVelocity(x=mats, elevation=elev, metrics='centroid', cores=4)
+#' plot(mc1$centroidVelocity, mc2$centroidVelocity,
+#' xlab='No elevation', ylab='Elevation')
+#' abline(0, 1)
 #' }
 #' 
 #' @export
@@ -579,26 +587,78 @@ bioticVelocity <- function(
 
 				}
 				
-				# statistics about cells
-				size <- nrow(x1) * ncol(x1)
-				x1ones <- x1 * 0 + 1
-				x2ones <- x2 * 0 + 1
-				x1x2ones <- x1ones * x2ones
-				numSharedNonNaCells <- cellStats(x1x2ones, 'sum')
-				propSharedCellsNotNA <- numSharedNonNaCells / size
-				timeFromPropNotNA <- cellStats(x1ones, 'sum')  / size
-				timeToPropNotNA <- cellStats(x2ones, 'sum')  / size
+				### mean abundance
+				if ('summary' %in% metrics) {
 
-				thisOut <- cbind(
-					thisOut,
-					data.frame(
-						propSharedCellsNotNA = propSharedCellsNotNA,
-						timeFromPropNotNA = timeFromPropNotNA,
-						timeToPropNotNA = timeToPropNotNA
-					),
-					row.names=NULL
-				)
+					# shared cells and NA cells
+					size <- nrow(x1) * ncol(x1)
+					x1ones <- x1 * 0 + 1
+					x2ones <- x2 * 0 + 1
+					x1x2ones <- x1ones * x2ones
+					numSharedNonNaCells <- cellStats(x1x2ones, 'sum')
+					propSharedCellsNotNA <- numSharedNonNaCells / size
+					timeFromPropNotNA <- cellStats(x1ones, 'sum')  / size
+					timeToPropNotNA <- cellStats(x2ones, 'sum')  / size
+
+					thisOut <- cbind(
+						thisOut,
+						data.frame(
+							propSharedCellsNotNA = propSharedCellsNotNA,
+							timeFromPropNotNA = timeFromPropNotNA,
+							timeToPropNotNA = timeToPropNotNA
+						),
+						row.names=NULL
+					)
 					
+					# mean
+					metric <- cellStats(x2, 'mean')
+					thisOut <- cbind(
+						thisOut,
+						data.frame(
+							mean = metric
+						),
+						row.names=NULL
+					)
+				
+					# sum
+					metric <- cellStats(x2, 'sum')
+					thisOut <- cbind(
+						thisOut,
+						data.frame(
+							sum = metric
+						),
+						row.names=NULL
+					)
+
+					# quantiles
+					metric <- raster::quantile(x2, quants, na.rm=TRUE)
+					
+					for (countQuant in seq_along(metric)) {
+					
+						thisQuantOut <- data.frame(
+							quantile = metric[countQuant]
+						)
+						
+						quantCharacter <- gsub(quants[countQuant], pattern='[.]', replacement='p')
+						names(thisQuantOut) <- paste0(names(thisQuantOut), '_quant', quantCharacter)
+						
+						thisOut <- cbind(thisOut, thisQuantOut, row.names=NULL)
+					
+					}
+
+					# prevalence
+					metric <- cellStats(x2 > 0, 'sum') / cellStats(x2ones, 'sum')
+					
+					thisOut <- cbind(
+						thisOut,
+						data.frame(
+							prevalence = metric
+						),
+						row.names=NULL
+					)
+					
+				}
+
 				### pre-calculate weighted longitude/latitude... used for centroid calculations for velocities
 				if (any(c('centroid', 'nsCentroid', 'ewCentroid', 'nCentroid', 'sCentriod', 'eCentroid', 'wCentroid', 'nsQuants', 'ewQuants') %in% metrics)) {
 
@@ -907,7 +967,7 @@ bioticVelocity <- function(
 						thisOut,
 						data.frame(
 							elevCentroidVelocity = metricRate,
-							elevCentroidElev = elev2
+							elevCentroidElev = x2centroidElev
 						),
 						row.names=NULL
 					)
@@ -976,60 +1036,6 @@ bioticVelocity <- function(
 					
 				}
 
-				
-				### mean abundance
-				if ('summary' %in% metrics) {
-
-					# mean
-					metric <- cellStats(x2, 'mean')
-					thisOut <- cbind(
-						thisOut,
-						data.frame(
-							mean = metric
-						),
-						row.names=NULL
-					)
-				
-					# sum
-					metric <- cellStats(x2, 'sum')
-					thisOut <- cbind(
-						thisOut,
-						data.frame(
-							sum = metric
-						),
-						row.names=NULL
-					)
-
-					# quantiles
-					metric <- raster::quantile(x2, quants, na.rm=TRUE)
-					
-					# remember
-					for (countQuant in seq_along(metric)) {
-					
-						thisQuantOut <- data.frame(
-							quantile = metric[countQuant]
-						)
-						
-						quantCharacter <- gsub(quants[countQuant], pattern='[.]', replacement='p')
-						names(thisQuantOut) <- paste0(names(thisQuantOut), '_quant', quantCharacter)
-						
-						thisOut <- cbind(thisOut, thisQuantOut, row.names=NULL)
-					
-					}
-
-					# prevalence
-					metric <- cellStats(x2 > 0, 'sum') / cellStats(x2ones, 'sum')
-					
-					thisOut <- cbind(
-						thisOut,
-						data.frame(
-							prevalence = metric
-						),
-						row.names=NULL
-					)
-					
-				}
-
 				### similarities
 				if ('similarity' %in% metrics) {
 					
@@ -1037,6 +1043,13 @@ bioticVelocity <- function(
 					x1x2diff <- x2 - x1
 					x1x2absDiff <- abs(x1x2diff)
 					x1x2prod <- x1 * x2
+
+					if (!exists('numSharedNonNaCells', inherits=FALSE)) {
+						x1ones <- x1 * 0 + 1
+						x2ones <- x2 * 0 + 1
+						x1x2ones <- x1ones * x2ones
+						numSharedNonNaCells <- cellStats(x1x2ones, 'sum')
+					}
 					
 					simpleMeanDiff <- cellStats(x1x2diff, 'sum') / numSharedNonNaCells
 					meanAbsDiff <- cellStats(x1x2absDiff, 'sum') / numSharedNonNaCells
