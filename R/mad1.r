e01904096c534451f64020ef86a74b12489c6d33
Adam Smith <asmith@mobot.org>
2022-06-11 23:51:58 -0500

    Update help

---
+++ R/mad1.r
@@ -0,0 +1,19 @@
+#' Madagascar spatial object
+#'
+#' SpatialPolygonsDataFrame of Madagascar from GADM
+#'
+#' @docType data
+#'
+#' @usage data(mad0)
+#'
+#' @format An object of class \code{'SpatialPolygonsDataFrame'}.
+#'
+#' @keywords datasets, Madagascar
+#'
+#' @source www.gadm.org
+#' 
+#' @examples
+#' data(mad1)
+#' mad1
+#' plot(mad1)
+'mad1'
